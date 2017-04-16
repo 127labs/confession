4c1be5cffba575109d3da44146d9e3f169d0a243
Imran Ismail <imran.codely@gmail.com>
2017-04-16 17:28:42 +0800

    Start Task supervisor

--- lib/confession/application.ex
+++ lib/confession/application.ex
@@ -4,7 +4,11 @@ defmodule Confession.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    children = [Confession.Endpoint.spec()]
+    children = [
+      Confession.Endpoint.spec(),
+      supervisor(Confession.Task, []),
+    ]
+
     opts = [strategy: :one_for_one, name: Confession.Supervisor]
 
     Supervisor.start_link(children, opts)
