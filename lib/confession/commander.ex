584ff245bc970d95d2a4aacff53952946269349f
Imran Ismail <imran.codely@gmail.com>
2017-04-19 17:40:54 +0800

    Fix compilation errors and warnings

--- lib/confession/commander.ex
+++ lib/confession/commander.ex
@@ -2,8 +2,8 @@ defmodule Confession.Commander do
   alias Messenger.Message
 
   def confess(confession, sender) do
-    Messenger.new
-    |> Messenger.put_text("""
+    Message.new
+    |> Message.put_text("""
       Please confirm if this is your confession?:
         #{confession}
     """)
@@ -12,15 +12,15 @@ defmodule Confession.Commander do
   end
 
   def about(sender) do
-    Messenger.new
-    |> Messenger.put_text(Confession.description)
+    Message.new
+    |> Message.put_text(Confession.description)
     |> Message.put_recipient(sender)
     |> Messenger.send_message!(async: :nolink)
   end
 
   def help(sender) do
-    Messenger.new
-    |> Messenger.put_text("""
+    Message.new
+    |> Message.put_text("""
       Available commands are:
         /help
         /about
@@ -31,8 +31,8 @@ defmodule Confession.Commander do
   end
 
   def default(sender) do
-    Messenger.new
-    |> Messenger.put_text("""
+    Message.new
+    |> Message.put_text("""
       To get started, please type "/confess <your-confession>" or "/help" for help
     """)
     |> Message.put_recipient(sender)
